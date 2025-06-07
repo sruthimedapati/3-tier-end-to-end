d6a967c5a3eff92ad96270ceed1b16c1c4cd7f21
sruthimedapati <medapatisruthi@gmail.com>
2025-06-07 13:44:39 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
  #Creating IAM role for EKS
   resource "aws_iam_role" "master" {
-    name = "veera-eks-master1"
+    name = "sruthi-eks-master"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "veera-eks-worker1"
+    name = "sruthi-eks-master"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -106,7 +106,7 @@ provider "aws" {
 
   resource "aws_iam_instance_profile" "worker" {
     depends_on = [aws_iam_role.worker]
-    name       = "veera-eks-worker-new-profile1"
+    name       = "sruthi-eks-worker-new-profile1"
     role       = aws_iam_role.worker.name
   }
  
